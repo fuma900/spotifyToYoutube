2accb21a0f6390d5499961e2d5d4ac7979ba3c2c
Marco Fumagalli <fuma900@gmail.com>
2014-11-12 16:43:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+function init () {
+	window.init();
+}
+
+var app = angular.module('app', ['serviceModule']);
+
+app.controller('appCtrl', ['$scope', '$http', '$q', '$window', 'Youtube', 'Helper', 'Spotify', 
+	function($scope, $http, $q, $window, Youtube, Helper, Spotify) {
+	$scope.gapiReady = false;
+	$scope.isLoggedIn = false;
+
+	$window.init= function() {
+		Youtube.setupApi();
+		$scope.gapiReady = true;
+		Youtube.checkAuth()
+		.then(function(authResult) {
+			if (authResult === true){
+				$scope.isLoggedIn = true;
+			} else {
+				$scope.isLoggedIn = false;
+			}
+		});
+	};
+
+	$scope.login = function() {
+		console.info('logging in');
+		Youtube.login()
+		.then(function(authResult) {
+			if (authResult === true){
+				$scope.isLoggedIn = true;
+			}
+		});
+	};
+
+	$scope.reset= function(){
+		$scope.urls = "";
+		$scope.results = [];
+		$scope.youtubeIds = [];
+	};
+
+	$scope.run = function (){
+		var tracks = [];
+		var youtubeIds = [];
+		var ids = Helper.extractSpotifyId($scope.urls);
+		var promises = Spotify.getTracks(ids);
+
+		$q.all(promises)
+		.then(function(results) {
+			angular.forEach(results, function(v,k){
+				tracks = tracks.concat(results[k].data.tracks);
+			});
+		})
+		.then(function() {
+			return Helper.getTracksInfo(tracks);
+		})
+		.then(function(tracks) {
+			var promises = [];
+			angular.forEach(tracks, function(track, i){
+				promises.push(Youtube.searchVideo(track));
+				// .then(function(response) {
+				// 	tracks[i].youtubeId = response.result.items[0].id.videoId;
+				// });
+			});
+			$q.all(promises)
+			.then(function(response) {
+				angular.forEach(tracks, function(track, i){
+					tracks[i].youtubeId = response[i].result.items[0].id.videoId;
+					youtubeIds.push(tracks[i].youtubeId);
+				});
+				$scope.results = tracks;
+				$scope.youtubeIds = youtubeIds;
+			});
+		});
+	};
+
+	$scope.urls = 	"http://open.spotify.com/track/0ifSeVGUr7py5GggttDhXw \nhttp://open.spotify.com/track/6c6OMbNVGHxGZWGc9ubHwq \nhttp://open.spotify.com/track/6gp3RHhdZlrPGaf5qcgFSg \nhttp://open.spotify.com/track/7pNC5ZIKtwUK0ReSpM3P9f";
+
+	// $scope.run();
+
+}]);
---
+++ services.js
@@ -0,0 +1,258 @@
+var services = angular.module('serviceModule', []);
+
+services.service('Youtube', ['$q', '$window', 'Helper', function($q, $window, Helper) {
+
+	this.clientId = '743605158714-vfjtaobdqlgshs7157r43e1ohntdkbe6.apps.googleusercontent.com';
+	this.scopes = 'https://www.googleapis.com/auth/youtube';
+
+	this.setupApi = function() {
+		gapi.client.setApiKey('AIzaSyBKB-CGWlr3id6q4SObfRPrYrAIHcACPuk');
+		$window.setTimeout(this.checkAuth,1);
+	};
+
+	this.checkAuth = function() {
+		var deferred = $q.defer();
+		gapi.auth.authorize({client_id: this.clientId, scope: this.scopes, immediate: true}, function(authResult) {
+			if (authResult && !authResult.error) {
+				deferred.resolve(true);
+			} else if (!authResult) {
+				deferred.resolve(false);
+			} else {
+				deferred.resolve(authResult.error);
+			}
+		});
+		return deferred.promise;
+	}
+
+	this.login = function(event) {
+		var deferred = $q.defer();
+        // Step 3: get authorization to use private data
+        gapi.auth.authorize({client_id: this.clientId, scope: this.scopes, immediate: false}, function(authResult) {
+        	if (authResult && !authResult.error) {
+				deferred.resolve(true);
+			} else if (!authResult) {
+				deferred.resolve(false);
+			} else {
+				deferred.resolve(authResult.error);
+			}
+        });
+        return deferred.promise;
+    }
+
+    this.createPlaylist = function() {
+    	var request = gapi.client.youtube.playlists.insert({
+    		part: 'snippet,status',
+    		resource: {
+    			snippet: {
+    				title: 'Test Playlist',
+    				description: 'A private playlist created with the YouTube API'
+    			},
+    			status: {
+    				privacyStatus: 'private'
+    			}
+    		}
+    	});
+    	request.execute(function(response) {
+    		var result = response.result;
+    		if (result) {
+    			playlistId = result.id;
+    		} else {
+				return false;    	
+    		}
+    	});
+    };
+
+	this.loadApi = function(callback) {
+		if(gapi.client){
+			if (!gapi.client.youtube){
+				gapi.client.load('youtube', 'v3', function() {
+					callback();
+				});
+			} else {
+				callback();
+			}
+		} else {
+			Helper.sleep(2000);
+			console.info('waiting for Google API to be loaded');
+			this.loadApi(callback);
+		}
+	};
+
+	this.searchVideo = function(track) {
+		var deferred = $q.defer();
+		this.loadApi(function() {
+			var q = track.artists + " " + track.name;
+			var request = gapi.client.youtube.search.list({
+				q: q,
+				part: 'snippet'
+			});
+			request.then(function(response) {
+				deferred.resolve(response);
+			});	
+		});
+		return deferred.promise;
+	};
+
+	this.searchVideos = function(tracks) {
+		var responses = [];
+		angular.forEach(tracks, function(track){
+			this.searchVideo(track)
+			.then(function(response) {
+				responses.push(response);
+			});
+		});
+		return $q.when(responses);
+	};
+}]);
+
+services.service('Spotify', ['$q', '$http', 'Helper', 'Youtube', function($q, $http, Helper, Youtube) {
+	this.getTracks = function (ids){
+		var chunks = [];
+		var promises = [];
+		var tracks = [];
+
+		if (ids.length > 1000){
+			return;
+		}
+
+		// Workaround to avoid limit of 50 tracks
+		if (ids.length > 50){
+			for (var i=0,j=ids.length; i<j; i+=50){
+				chunks.push(ids.slice(i,i+50));
+			}
+		} else {
+			chunks = [ids];
+		}
+		
+		for (var k=0; k<chunks.length; k+=1) {
+			promises.push(
+				$http.get('https://api.spotify.com/v1/tracks', {
+					params: {ids: chunks[k].join()},
+				})
+			);
+		}
+		return promises;
+	};
+}]);
+
+services.service('Helper', ['$q', function($q) {
+	this.extractSpotifyId = function (urls){
+		var ids = [];
+		urls = urls.split(/\n/);
+
+		angular.forEach(urls, function(url){
+			var type = 0;								// 0: not set, 1: http, 2: spotify uri
+			var a = url.trim().split(':');			// Split lines and remove whitespaces
+			// Check if the url is http link or spotify uri
+			type = (a[0] === 'http') ? ( 
+					1
+				) : (
+					(a[0] === 'spotify')?2:0
+				);
+			if (type === 0){
+				console.log('Url ' + url.trim() + ' is not valid!');
+			} else {
+				var id = a[a.length - 1].split('/');
+				id = id[id.length - 1];
+				ids.push(id);
+			}
+		});
+
+		return ids;
+	};
+
+	this.getTracksInfo = function(tracks) {
+		var temp = [];
+
+		console.log(tracks);
+		for (var i=0; i<tracks.length; i+=1){
+			if (tracks[i]){
+				var artists = [];
+				for (var j=0; j<tracks[i].artists.length; j+=1){
+					artists.push(tracks[i].artists[j].name);
+				}
+				artists = artists.join(', ');
+				temp.push({
+					'album': 	tracks[i].album,
+					'artists': 	artists,
+					'name': 	tracks[i].name,
+				});
+			}
+		}
+
+		return $q.when(temp);
+	};
+
+	this.sleep = function (milliseconds) {
+	  var start = new Date().getTime();
+	  for (var i = 0; i < 1e7; i++) {
+	    if ((new Date().getTime() - start) > milliseconds){
+	      break;
+	    }
+	  }
+	};
+}]);
+
+services.directive('youtube', ['$window', function($window) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "="
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+
+        player = new YT.Player(element.children()[0], {
+          playerVars: {
+            autoplay: 0,
+            html5: 1,
+            theme: "light",
+            modesbranding: 1,
+            color: "white",
+            iv_load_policy: 3,
+            showinfo: 1,
+            controls: 1,
+            // playlist: scope.videoid.join(),
+          },
+          height: 300,
+          width: 570,
+        });
+
+      }
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        console.log(newValue);
+        player.cuePlaylist({
+        	playlist: scope.videoid,
+        });
+
+      }); 
+
+      scope.$watch('height + width', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        player.setSize(scope.width, scope.height);
+
+      });
+    }  
+  };
+}]);
